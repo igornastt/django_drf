56db886c40b9126a9f64e12cb2a3bdef9553d981
igornast <grezle00@gmail.com>
2023-10-11 08:15:35 +0300

    drf-yasg установлено в файл settings.py, создана схема документации, добавлены зависимости.

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
+    'drf_yasg',
 
     'users',
     'courses',
@@ -151,3 +152,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
+
+STRIPE_API_KEY = os.getenv('STRIPE_API_KEY')
